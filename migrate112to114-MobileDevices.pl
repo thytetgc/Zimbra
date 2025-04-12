a7fccfdc2d431fb39d8c43156db800f1b7e84aa6
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2025-04-12 17:29:02 -0300

    Update migrate112to114-MobileDevices.pl

--- migrate112to114-MobileDevices.pl
+++ migrate112to114-MobileDevices.pl
@@ -10,8 +10,16 @@ my $toVersion = 114;
 Migrate::verifySchemaVersion($fromVersion);
 
 eval {
+    my $mysql_pass = get_mysql_password();
+
+    # Verifica a versão atual no banco antes de seguir
+    my $current_version = get_db_version($mysql_pass);
+    if ($current_version != $fromVersion) {
+        die "Versão atual do banco é $current_version. Esperado: $fromVersion. Abortando update.";
+    }
+
     # Verifica e adiciona a coluna mobile_operator
-    if (!columnExists("mobile_devices", "mobile_operator")) {
+    if (!columnExists("mobile_devices", "mobile_operator", $mysql_pass)) {
         Migrate::log("Adding 'mobile_operator' column to mobile_devices");
         my $sql = "ALTER TABLE mobile_devices ADD COLUMN mobile_operator VARCHAR(512);";
         Migrate::runSql($sql);
@@ -20,7 +28,7 @@ eval {
     }
 
     # Verifica e adiciona a coluna last_updated_by
-    if (!columnExists("mobile_devices", "last_updated_by")) {
+    if (!columnExists("mobile_devices", "last_updated_by", $mysql_pass)) {
         Migrate::log("Adding 'last_updated_by' column to mobile_devices");
         my $sql = "ALTER TABLE mobile_devices ADD COLUMN last_updated_by ENUM('Admin','User') DEFAULT 'User';";
         Migrate::runSql($sql);
@@ -29,6 +37,7 @@ eval {
     }
 
     # Atualiza a versão do schema no banco
+    Migrate::log("Updating DB schema version from $fromVersion to $toVersion.");
     Migrate::updateSchemaVersion($fromVersion, $toVersion);
     Migrate::log("Database schema updated from $fromVersion to $toVersion successfully.");
 };
@@ -41,12 +50,9 @@ if ($@) {
 exit(0);
 
 # ---------------------
-# Verifica se a coluna existe
+# Função para verificar se uma coluna existe
 sub columnExists {
-    my ($table, $column) = @_;
-
-    my $mysql_pass = qx(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password);
-    chomp($mysql_pass);
+    my ($table, $column, $mysql_pass) = @_;
 
     my $sql = qq{
         SELECT COUNT(*) FROM information_schema.COLUMNS
@@ -56,6 +62,25 @@ sub columnExists {
     my $cmd = qq(/opt/zimbra/bin/mysql -u zimbra -p$mysql_pass -N -e "$sql");
     my $result = qx($cmd);
     chomp($result);
-
     return $result > 0;
+}
+
+# ---------------------
+# Pega a senha do MySQL de forma limpa
+sub get_mysql_password {
+    my $line = qx(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password);
+    chomp($line);
+    $line =~ s/^.*\s//; # Pega apenas o valor
+    return $line;
+}
+
+# ---------------------
+# Pega a versão atual do banco
+sub get_db_version {
+    my ($mysql_pass) = @_;
+    my $sql = "SELECT value FROM config WHERE name='db.version';";
+    my $cmd = qq(/opt/zimbra/bin/mysql -u zimbra -p$mysql_pass -N -e "$sql" zimbra);
+    my $result = qx($cmd);
+    chomp($result);
+    return $result;
 }
