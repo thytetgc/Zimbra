89b01c0a5e403cb35c18956a26abef5d78d2c869
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2025-04-12 10:10:00 -0300

    Update zimbra_upgrade_patch_MobileDevices.sh

--- zimbra_upgrade_patch_MobileDevices.sh
+++ zimbra_upgrade_patch_MobileDevices.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Mostrar as senhas
+/opt/zimbra/bin/zmlocalconfig -s | grep mysql | grep password
+
+/opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password) -e "DESC mobile_devices;" zimbra | grep mobile_operator
+
 
 # Função para checar se a coluna 'mobile_operator' existe
 check_column_exists() {
     echo "Verificando se a coluna 'mobile_operator' existe na tabela 'mobile_devices'..."
-    result=$(/opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey mysql_root_password | awk '{print $2}') -e "SHOW COLUMNS FROM zimbra.mobile_devices LIKE 'mobile_operator';" -s -N)
+    result=$(/opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password) -e "SHOW COLUMNS FROM zimbra.mobile_devices LIKE 'mobile_operator';" -s -N)
 
     if [[ -z "$result" ]]; then
         echo "Coluna 'mobile_operator' não existe. Adicionando..."
@@ -15,7 +20,7 @@ check_column_exists() {
 
 # Função para adicionar a coluna 'mobile_operator'
 add_column() {
-    /opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey mysql_root_password | awk '{print $2}') -e "ALTER TABLE zimbra.mobile_devices ADD COLUMN mobile_operator VARCHAR(512);"
+    /opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password) -e "ALTER TABLE zimbra.mobile_devices ADD COLUMN mobile_operator VARCHAR(512);"
     if [[ $? -eq 0 ]]; then
         echo "Coluna 'mobile_operator' adicionada com sucesso!"
     else
@@ -27,7 +32,7 @@ add_column() {
 # Função para atualizar o schema para versão 113
 update_schema_version() {
     echo "Atualizando o schema para versão 113..."
-    /opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey mysql_root_password | awk '{print $2}') -e "UPDATE zimbra.schema_version SET value = '113' WHERE name = 'version';"
+    /opt/zimbra/bin/mysql -u zimbra -p$(/opt/zimbra/bin/zmlocalconfig -s -m nokey zimbra_mysql_password) -e "UPDATE zimbra.schema_version SET value = '113' WHERE name = 'version';"
     if [[ $? -eq 0 ]]; then
         echo "Schema atualizado para versão 113 com sucesso!"
     else
